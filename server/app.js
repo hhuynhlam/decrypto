54c23b88455f89272b04801c0e4bcb0f2fd9c215
Hai Huynhlam <hhuynhlam@gmail.com>
2020-06-30 10:21:51 -0700

    working server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const path = require('path')
 const config = require('./config')
 
 const app = express()
 
-app.use('static', express.static('build'))
-app.use('static', express.static('public'))
+app.use(express.static(path.join(__dirname, '../build')))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../build/index.html')))
 
 module.exports = app
